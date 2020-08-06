c11a25ca4310f5e88db31aeca2683588aaf35785
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2020-08-06 15:10:50 -0300

    v.0.1.9
    - Criação do componente Textarea e sua estilização.
    - Criação do componente Select e sua estilização.
    - Correção de palavras escritas erradamente.

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm () {
@@ -18,7 +21,61 @@ function TeacherForm () {
                     <Input name='name' label='Nome completo' />
                     <Input name='avatar' label='Avatar' />
                     <Input name='whatsapp' label='Whatsapp' />
+                    <Textarea name='bio' label='Biografia' />
                 </fieldset>
+                
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select
+                        name='subject'
+                        label='Matéria'
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' }
+                        ]}    
+                    />
+                    <Input name='cost' label='Custo da aula por hora' />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type='button'>+ Novo horário</button>
+                    </legend>
+
+                    <div className="schedule-item">
+                        <Select
+                            name='week_day'
+                            label='Dia da semana'
+                            options={[
+                                { value: '0', label: 'Doming' },
+                                { value: '1', label: 'Segunda-feira' },
+                                { value: '2', label: 'Terça-feira' },
+                                { value: '3', label: 'Quarta-feira' },
+                                { value: '4', label: 'Quinta-feira' },
+                                { value: '5', label: 'Sexta-feira' },
+                                { value: '6', label: 'Sábado' }
+                            ]}    
+                        />
+                        <Input name='from' label='Das' type='time' />
+                        <Input name='to' label='Até as' type='time' />
+                    </div>
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={ warningIcon } alt="Ícone de aviso"/>
+                        Importante! <br/>
+                        Preencha todos os dados
+                    </p>
+                    <button type='button'>
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
         </div>
     );
